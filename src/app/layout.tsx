6567385f1121fe9714aa13021c635137c52475ec
Mansour Mbengue <mbgmansour13@gmail.com>
2025-09-07 23:42:41 +0200

    feat: refactor RootLayout to include Providers component and update children prop handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Aero Drop",
   description: "a decentralized airdrop platform",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(props: {children: ReactNode}) {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
+      <body>test baby
+        <Providers>
+          {props.children}
+        </Providers>
       </body>
     </html>
   );
