4bdd84d4e722c632cfa9cc844432d0b762723fde
Mansour Mbengue <mbgmansour13@gmail.com>
2025-09-07 23:42:32 +0200

    feat: integrate QueryClientProvider into Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,14 +4,18 @@ import { ReactNode } from "react";
 import config from "@/rainbowKitConfig";
 import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export function Providers(props: {children: ReactNode}) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <WagmiProvider config={config}>
-      <RainbowKitProvider>
-        {props.children}
-      </RainbowKitProvider>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {props.children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   )
 }
