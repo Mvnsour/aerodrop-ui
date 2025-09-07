f92048c055ad7e62c62b6d5b9decefaf155a52ca
Mansour Mbengue <mbgmansour13@gmail.com>
2025-09-07 23:28:19 +0200

    feat: add Providers component with Wagmi and RainbowKit integration

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { ReactNode } from "react";
+import config from "@/rainbowKitConfig";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+
+export function Providers(props: {children: ReactNode}) {
+  return (
+    <WagmiProvider config={config}>
+      <RainbowKitProvider>
+        {props.children}
+      </RainbowKitProvider>
+    </WagmiProvider>
+  )
+}
